a68c368cfbf8e787ceb047bd0e94254f993a5c33
FelixPog <felix.p94380@live.fr>
2020-12-19 00:31:18 -0500

    Create first version of animation instance for a Third Person Chararcter : Idle, Jog, Crouch, Jog, Jump

---
+++ Source/SurvivorSandbox/SurvivorSandboxGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurvivorSandboxGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASurvivorSandboxGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASurvivorSandboxGameMode();
+};
+
+
+
